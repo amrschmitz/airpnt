89f2a333241d2d3bd16a7d8868316e908f9c9f43
Alexandre Schmitz <alexandre.schmitz@cerbo.me>
2018-11-12 16:08:54 -0500

    created the plant model

---
+++ db/migrate/20181112210556_create_plants.rb
@@ -0,0 +1,11 @@
+class CreatePlants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :plants do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
