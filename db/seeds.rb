9930c4fc3d7d2bf2e6a04a9ba6380bc80a1eebf1
Alexandre Schmitz <alexandre.schmitz@cerbo.me>
2018-11-12 19:22:58 -0500

    seeded the db and created some views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Cleaning database...'
+User.destroy_all
+Plant.destroy_all
+
+puts 'Creating users...'
+
+users_attributes = []
+
+10.times do
+  users_attributes << {
+                        email: Faker::Internet.email,
+                        password: Faker::Internet.password(8),
+                      }
+end
+
+users_attributes.each do |user_hash|
+  User.create!(user_hash)
+end
+
+puts 'Creating plants...'
+
+plants_attributes = []
+
+User.all.each do |user|
+  2.times do
+    plants_attributes << {
+                          name: Faker::Food.vegetables,
+                          description: Faker::Lorem.paragraph,
+                          user_id: user.id
+                        }
+  end
+end
+
+plants_attributes.each do |plant_hash|
+  Plant.create!(plant_hash)
+end
+
+puts 'Finished!'
