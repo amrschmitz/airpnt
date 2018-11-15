1f1b1304fc94170bd7ec41651ccb009db7aba6f4
John Freeman <Johngrahamfreeman@gmail.com>
2018-11-15 15:09:07 -0500

    history fuck yeah

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Dependencies
   has_many :plants
   has_many :carings
+  has_many :requested_carings, through: :plants, source: :carings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
