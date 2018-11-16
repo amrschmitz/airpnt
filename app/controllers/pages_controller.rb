1d2f2fae827bce59ca236dd1ab0a0dae5a5c1311
Alexandre Schmitz <alexandre.schmitz@cerbo.me>
2018-11-16 15:06:09 -0500

    first try

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   skip_after_action :verify_authorized
+  layout 'search_nav'
 
   def landing
     if params[:query].present?
