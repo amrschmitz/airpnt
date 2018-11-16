7fcfa42d7cca11c6b7e72ebb5b42c9162e7da24e
John Freeman <Johngrahamfreeman@gmail.com>
2018-11-16 11:53:19 -0500

    Added PG_search to plants model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -6,6 +6,13 @@ class Plant < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   
+  include PgSearch
+  pg_search_scope :search_by_title_and_syllabus,
+    against: [ :title, :syllabus ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
   def owners
     carings.pluck(:user)
   end
